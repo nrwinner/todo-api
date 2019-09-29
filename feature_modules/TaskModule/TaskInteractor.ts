e0175bcbfcbc7a9eca4631bc6912782ccd5d1c66
Nick Winner <design@winnerdigital.net>
2019-09-29 19:16:55 -0400

    :lock: Use the route's username in the query for tasks

--- feature_modules/TaskModule/TaskInteractor.ts
+++ feature_modules/TaskModule/TaskInteractor.ts
@@ -2,30 +2,35 @@ import { TaskDataStore } from "./TaskDataStore";
 import { Task } from "../../types/Task";
 import { TaskQuery } from "../../types/Query";
 import { TaskMongoDriver } from "./TaskMongoDriver";
+import { RequestErrorType } from "../../errors/Error";
 
 export class TaskInteractor {
   constructor(private dataStore: TaskDataStore = new TaskMongoDriver()) { }
 
-  async createTask(task: Partial<Task>): Promise<string> {
+  async createTask(task: Partial<Task>, author: string): Promise<string> {
+    if (task.author !== author) {
+      throw RequestErrorType.MISMATCHED_PROPERTY('The author of the task does not match the the username in the route.');
+    }
+
     const resultId = await this.dataStore.createTask(task);
     return resultId;
   }
   
-  async updateTask(task: Partial<Task>): Promise<void> {
-    await this.dataStore.updateTask(task);
+  async updateTask(task: Partial<Task>, author: string): Promise<void> {
+    await this.dataStore.updateTask(task, author);
   }
   
-  async getTask(taskId: string): Promise<Task> {
-    const task = await this.dataStore.getTask(taskId);
+  async getTask(taskId: string, author: string): Promise<Task> {
+    const task = await this.dataStore.getTask(taskId, author);
     return task;
   }
   
-  async deleteTask(taskId: string): Promise<void> {
-    await this.dataStore.deleteTask(taskId);
+  async deleteTask(taskId: string, author: string): Promise<void> {
+    await this.dataStore.deleteTask(taskId, author);
   }
   
-  async getTasks(query: TaskQuery): Promise<Task[]> {
-    const tasks = await this.dataStore.getTasks(query);
+  async getTasks(query: TaskQuery, author: string): Promise<Task[]> {
+    const tasks = await this.dataStore.getTasks(query, author);
     return tasks;
   }
 }
